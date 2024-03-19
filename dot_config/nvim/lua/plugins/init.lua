80098841696d4b1b7461dedae0cf92f165b875a9
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-03-19 12:11:51 +0900

    Fixed performance issue due to nvim-dap

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -437,7 +437,15 @@ local pluginlist = {
     },
     {
         "mfussenegger/nvim-dap",
-        event = "VeryLazy",
+        keys = {
+  { "<F5>", function() require'dap'.continue() end, desc = "Debug: Start/Continue" },
+  { "<F1>", function() require'dap'.step_into() end, desc = "Debug: Step Into" },
+  { "<F2>", function() require'dap'.step_over() end, desc = "Debug: Step Over" },
+  { "<F3>", function() require'dap'.step_out() end, desc = "Debug: Step Out" },
+  { "<leader>b", function() require'dap'.toggle_breakpoint() end, desc = "Debug: Toggle Breakpoint" },
+  { "<leader>B", function() require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Debug: Set Breakpoint with Condition" }
+},
+
         dependencies = {
             -- Creates a beautiful debugger UI
             {
