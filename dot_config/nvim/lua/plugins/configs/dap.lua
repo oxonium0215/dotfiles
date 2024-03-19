a97880ef92e9aba439276b15997f1d0f92dd9d09
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-03-19 12:11:51 +0900

    Fixed performance issue due to nvim-dap

--- dot_config/nvim/lua/plugins/configs/dap.lua
+++ dot_config/nvim/lua/plugins/configs/dap.lua
@@ -36,20 +36,6 @@ dap.configurations = {
     }
 }
 
-vim.keymap.set("n", "<F5>", dap.continue, {desc = "Debug: Start/Continue"})
-vim.keymap.set("n", "<F1>", dap.step_into, {desc = "Debug: Step Into"})
-vim.keymap.set("n", "<F2>", dap.step_over, {desc = "Debug: Step Over"})
-vim.keymap.set("n", "<F3>", dap.step_out, {desc = "Debug: Step Out"})
-vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {desc = "Debug: Toggle Breakpoint"})
-vim.keymap.set(
-    "n",
-    "<leader>B",
-    function()
-        dap.set_breakpoint(vim.fn.input "Breakpoint condition: ")
-    end,
-    {desc = "Debug: Set Breakpoint"}
-)
-
 dapui.setup {
     icons = {expanded = "▾", collapsed = "▸", current_frame = "*"},
     controls = {
