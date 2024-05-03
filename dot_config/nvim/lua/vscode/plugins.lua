b6d60a5507fa38a52d2de591183b0b80c5b60d97
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-05-03 22:39:07 +0900

    [Add] Added rall template to my_template.cpp

--- dot_config/nvim/lua/vscode/plugins.lua
+++ dot_config/nvim/lua/vscode/plugins.lua
@@ -4,22 +4,6 @@ local pluginlist = {
     -- ╭─────────────────────────────────────────────────────────────────────────────────╮
     -- │ ∘ Other                                                                         │
     -- ╰─────────────────────────────────────────────────────────────────────────────────╯
-    {
-        "yorickpeterse/nvim-window",
-        init = function()
-            require("core.utils").load_mappings "nvimwindow"
-        end
-    },
-    {
-        "m4xshen/hardtime.nvim",
-        event = {"BufReadPost", "BufAdd", "BufNewFile"},
-        dependencies = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim"},
-        opts = {
-            disable_mouse = false,
-            disabled_filetypes = {"qf", "alpha", "NvimTree", "lazy", "mason", "oil", "toggleterm"},
-            max_count = 10,
-        }
-    },
     -- Lua Library
     {"nvim-lua/popup.nvim"},
     {"nvim-lua/plenary.nvim"},
@@ -28,14 +12,6 @@ local pluginlist = {
     -- ╭─────────────────────────────────────────────────────────────────────────────────╮
     -- │ ∘ UI                                                                            │
     -- ╰─────────────────────────────────────────────────────────────────────────────────╯
-    -- Notify
-    {
-        "vigoux/notifier.nvim",
-        event = "VeryLazy",
-        config = function()
-            require "notifier".setup {}
-        end
-    },
     -- Font
     {
         "nvim-tree/nvim-web-devicons",
@@ -91,87 +67,6 @@ local pluginlist = {
             require("Comment").setup(opts)
         end
     },
-    -- ╭─────────────────────────────────────────────────────────────────────────────────╮
-    -- │ ∘ Telescope                                                                     │
-    -- ╰─────────────────────────────────────────────────────────────────────────────────╯
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            {
-                "nvim-lua/plenary.nvim"
-            },
-            {
-                "Allianaab2m/telescope-kensaku.nvim",
-                config = function()
-                    require("telescope").load_extension("kensaku") -- :Telescope kensaku
-                end
-            },
-            {
-                "nvim-treesitter/nvim-treesitter"
-            },
-            {
-                "nvim-telescope/telescope-github.nvim",
-                config = function()
-                    require("telescope").load_extension("gh")
-                end
-            },
-            {
-                "nvim-telescope/telescope-ui-select.nvim",
-                config = function()
-                    require("telescope").load_extension("ui-select")
-                end
-            },
-            {
-                "LinArcX/telescope-changes.nvim",
-                config = function()
-                    require("telescope").load_extension("changes")
-                end
-            },
-            {
-                "nvim-telescope/telescope-live-grep-args.nvim",
-                config = function()
-                    require("telescope").load_extension("live_grep_args")
-                end
-            },
-            {
-                "nvim-telescope/telescope-smart-history.nvim",
-                config = function()
-                    require("telescope").load_extension("smart_history")
-                end,
-                build = function()
-                    os.execute("mkdir -p " .. vim.fn.stdpath("state") .. "databases/")
-                end
-            },
-            {
-                "nvim-telescope/telescope-symbols.nvim"
-            },
-            {
-                "debugloop/telescope-undo.nvim",
-                config = function()
-                    require("telescope").load_extension("undo")
-                end
-            }
-        },
-        cmd = "Telescope",
-        init = function()
-            require("core.utils").load_mappings "telescope"
-        end,
-        opts = function()
-            return require "plugins.configs.telescope"
-        end,
-        config = function(_, opts)
-            local telescope = require "telescope"
-            telescope.setup(opts)
-
-            -- load extensions
-            for _, ext in ipairs(opts.extensions_list) do
-                telescope.load_extension(ext)
-            end
-        end
-    },
-    -- ╭─────────────────────────────────────────────────────────────────────────────────╮
-    -- │ ∘ Filer & Terminal                                                              │
-    -- ╰─────────────────────────────────────────────────────────────────────────────────╯
 }
 
 local lazyconfig = require("plugins.configs.lazy_nvim")
