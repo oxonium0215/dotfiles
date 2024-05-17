a90e4e0f80a9a87c8549514602fa5f0f1384e813
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-05-17 23:42:03 +0900

    [Fix] Fixed some deplicated apis and changed notify plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -71,6 +71,34 @@ local pluginlist = {
         cmd = {"SearchBoxIncSearch", "SearchBoxReplace"},
         dependencies = {"MunifTanjim/nui.nvim"}
     },
+    {
+  "rcarriga/nvim-notify",
+  keys = {
+    {
+      "<leader>un",
+      function()
+        require("notify").dismiss({ silent = true, pending = true })
+      end,
+      desc = "Dismiss All Notifications",
+    },
+  },
+  opts = {
+    stages = "static",
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  },
+  init = function()
+        vim.notify = require("notify")
+  end,
+},
     -- Cmdline
     --[[
     {
@@ -92,14 +120,6 @@ local pluginlist = {
         dependencies = {"MunifTanjim/nui.nvim"},
     },
     ]]
-    -- Notify
-    {
-        "vigoux/notifier.nvim",
-        event = "VeryLazy",
-        config = function()
-            require "notifier".setup {}
-        end
-    },
     -- Font
     {
         "nvim-tree/nvim-web-devicons",
@@ -332,9 +352,12 @@ local pluginlist = {
     },
     {
         "j-hui/fidget.nvim",
-        tag = "legacy",
-        event = "LspAttach",
-        opts = {}
+        event = "VeryLazy",
+        opts = {
+			notification = {
+				-- override_vim_notify = true,
+			},
+        },
     },
     {
         "numToStr/Comment.nvim",
@@ -420,7 +443,6 @@ local pluginlist = {
             },
             {
                 "weilbith/nvim-lsp-smag",
-                after = "nvim-lspconfig"
             }
         }
     },
@@ -568,7 +590,7 @@ local pluginlist = {
             "ToggleTermSendCurrentLine",
             "ToggleTermSendVisualSelection"
         },
-        config = require("plugins.configs.toggleterm")
+        opts = require("plugins.configs.toggleterm")
     },
     -- ╭─────────────────────────────────────────────────────────────────────────────────╮
     -- │ ∘ Code Runner & Live server                                                     │
