c48fbd47736e32c222e38d50ad925a06b9662d59
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-05-19 09:02:24 +0900

    [Fix] Fixed bufferline diagnostics icon

--- dot_config/nvim/lua/plugins/configs/bufferline.lua
+++ dot_config/nvim/lua/plugins/configs/bufferline.lua
@@ -9,24 +9,21 @@ local opts = {
                 text_align = "left"
             }
         },
-        indicator_icon = "▎",
-        modified_icon = "●",
-        buffer_close_icon = "",
-        close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
+--        indicator_icon = "▎",
+--        modified_icon = "●",
+--        buffer_close_icon = "",
+--        close_icon = "",
+--        left_trunc_marker = "",
+--        right_trunc_marker = "",
         numbers = "ordinal",
-        max_name_length = 15,
-        max_prefix_length = 6,
+--        max_name_length = 15,
+--        max_prefix_length = 6,
         diagnostics = "nvim_lsp",
-        show_buffer_icons = true,
-        show_buffer_close_icons = false,
-        show_close_icon = true,
-        persist_buffer_sort = true,
-        enforce_regular_tabs = true,
-        diagnostics_indicator = function(count, level)
-            local icon = level:match("error") and "" or ""
-            return icon .. count
+        diagnostics_indicator = function(_, _, diag)
+            local icons = require('core.icons').get('diagnostics')
+            local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+              .. (diag.warning and icons.Warn .. diag.warning or "")
+            return vim.trim(ret)
         end,
     }
 }
