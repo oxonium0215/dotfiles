17613e362dc7617669d290db70e7532d64e59204
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-05-22 22:56:31 +0900

    [Add] Added dressing.nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -99,6 +99,21 @@ local pluginlist = {
             vim.notify = vim.schedule_wrap(require("notify"))
         end,
     },
+    {
+        "stevearc/dressing.nvim",
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
     -- Cmdline
     --[[
     {
