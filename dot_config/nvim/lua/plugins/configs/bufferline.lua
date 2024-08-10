4513638ab50dd2e3d341ef9be67909bdfde9cd6b
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-08-11 08:58:55 +0900

    [Add] Added toggle theme button to bufferline

--- dot_config/nvim/lua/plugins/configs/bufferline.lua
+++ dot_config/nvim/lua/plugins/configs/bufferline.lua
@@ -1,3 +1,15 @@
+--dofile(vim.g.base46_cache .. "bufferline")
+
+vim.cmd [[
+function! Toggle_theme(a,b,c,d)
+    lua require('base46').toggle_theme()
+endfunction
+
+function! Quit_vim(a,b,c,d)
+    qa
+endfunction
+]]
+
 local opts = {
     options = {
         always_show_bufferline = true,
@@ -9,15 +21,20 @@ local opts = {
                 text_align = "left"
             }
         },
---        indicator_icon = "▎",
---        modified_icon = "●",
---        buffer_close_icon = "",
---        close_icon = "",
---        left_trunc_marker = "",
---        right_trunc_marker = "",
+        modified_icon = "●",
+        buffer_close_icon = "󰅖",
+        show_close_icon = false,
+        left_trunc_marker = " ",
+        right_trunc_marker = " ",
         numbers = "ordinal",
---        max_name_length = 15,
---        max_prefix_length = 6,
+        max_name_length = 20,
+        max_prefix_length = 13,
+        tab_size = 20,
+        show_tab_indicators = true,
+        enforce_regular_tabs = false,
+        show_buffer_close_icons = true,
+        separator_style = "thin",
+        themable = true,
         diagnostics = "nvim_lsp",
 --        diagnostics_indicator = function(_, _, diag)
 --            local icons = require('core.icons').get('diagnostics')
@@ -25,6 +42,14 @@ local opts = {
 --              .. (diag.warning and icons.Warn .. diag.warning or "")
 --            return vim.trim(ret)
 --        end,
+        custom_areas = {
+            right = function()
+                return {
+                    { text = "%@Toggle_theme@" .. "   " .. "%X" },
+                    { text = "%@Quit_vim@󰅖 %X" }
+                }
+            end,
+        }
     }
 }
 
