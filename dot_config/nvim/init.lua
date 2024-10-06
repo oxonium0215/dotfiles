15ac2a03fb497e56e537add53a1e79df9c23e775
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2024-10-06 22:20:10 +0900

    Revert "add: add nvchad ui/base46 plugin"
    This reverts commit d8a846dab33db37680953fd0d41c61cd11672df9.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,7 +3,6 @@ if vim.loader then vim.loader.enable() end
 require("config")
 require("core.utils").set_mappings("general")
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
 
 -- bootstrap lazy.nvim!
 if not vim.loop.fs_stat(lazypath) then
@@ -16,7 +15,4 @@ if vim.g.vscode then
 else
     require("plugins")
     -- vim.cmd("colorscheme onedark")
-    dofile(vim.g.base46_cache .. "syntax")
-    dofile(vim.g.base46_cache .. "defaults")
-    dofile(vim.g.base46_cache .. "statusline")
 end
