9eb247e946bfb00689f3ac9349b4c2f13aa91dfa
oxonium0215 <yugo@LAPTOP-CSLUPIM6>
2025-04-17 16:29:32 +0900

    fix: remove cppcheck for c filetype

--- dot_config/nvim/lua/plugins/configs/lsp/languages/c.lua
+++ dot_config/nvim/lua/plugins/configs/lsp/languages/c.lua
@@ -1,8 +1,19 @@
 return {
-  lsp = {
-    servers = { "clangd" },
-    formatters = { "clang-format" },
-    linters = { "cppcheck" }
-  },
-  treesitter = { "c" }
+	lsp = {
+		servers = { "clangd" },
+		setup = {
+			clangd = function()
+				return {
+					cmd = { "clangd", "--offset-encoding=utf-16", "--background-index", "--clang-tidy" },
+					init_options = {
+						clangdFileStatus = true,
+						usePlaceholders = true,
+						completeUnimported = true,
+						semanticHighlighting = true,
+					},
+				}
+			end,
+		},
+	},
+	treesitter = { "c" },
 }
