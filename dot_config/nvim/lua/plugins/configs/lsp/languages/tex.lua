4abd4c2da0ce9fbd7219bad516bd6c84aa1e85a7
oxonium0215 <yugo@LAPTOP-CSLUPIM6.localdomain>
2025-06-13 00:29:51 +0900

    fix: fix japanese document support

--- dot_config/nvim/lua/plugins/configs/lsp/languages/tex.lua
+++ dot_config/nvim/lua/plugins/configs/lsp/languages/tex.lua
@@ -2,7 +2,55 @@ return {
   lsp = {
     servers = { "texlab" },
     formatters = { "latexindent" },
-    linters = { "chktex" }
+    linters = { "chktex" },
+    setup = {
+      texlab = function()
+        return {
+          settings = {
+            texlab = {
+              build = {
+                executable = "latexmk",
+                args = {
+                  "-lualatex",
+                  "-synctex=1",
+                  "-interaction=nonstopmode",
+                  "-file-line-error",
+                  "%f"
+                },
+                onSave = true,
+                forwardSearchAfter = false,  -- Disable to avoid conflicts
+              },
+              forwardSearch = {
+                executable = "SumatraPDF.exe",
+                args = {
+                  "-reuse-instance",
+                  "-inverse-search",
+                  "\"nvim --headless -c \\\"VimtexInverseSearch %l '%f'\\\"\"",
+                  "%p"
+                }
+              },
+              completion = {
+                triggerCharacters = { "\\", "{", "}", "[", "]" }
+              },
+              diagnostics = {
+                allowedPatterns = { ".*" },
+                ignoredPatterns = {
+                  "^Overfull \\\\hbox.*",
+                  "^Underfull \\\\hbox.*",
+                  ".*will be scaled.*",
+                  ".*luatexja-fontspec Warning.*"
+                }
+              },
+              -- Enhanced Japanese support
+              experimental = {
+                followPackageLinks = true,
+                mathEnvironments = { "align", "equation", "gather" }
+              }
+            }
+          }
+        }
+      end
+    }
   },
   treesitter = { "latex", "bibtex" }
 }
