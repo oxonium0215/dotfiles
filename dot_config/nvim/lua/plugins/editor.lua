0de1e782c97bcb63724ee3771d4b0825009c1018
oxonium0215 <102494177+oxonium0215@users.noreply.github.com>
2026-09-06 16:36:15 +0900

    fix(nvim): rename overseer template to avoid chezmoi script collision

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -152,7 +152,7 @@ return {
       "OverseerClearCache",
     },
     opts = {
-      templates = { "builtin", "user.cpp_build", "user.run_script" },
+      templates = { "builtin", "user.cpp_build", "user.script_runner" },
       strategy = { "toggleterm" },
     },
     dependencies = {
